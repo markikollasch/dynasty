70d0247250892803bb4a4a64a55e723650cddf49
Mark Kollasch <markikollasch@gmail.com>
2016-01-07 00:42:08 -0600

    Add measurement of elapsed time.

--- src/Interface/InterfaceState.hs
+++ src/Interface/InterfaceState.hs
@@ -7,11 +7,12 @@ module Interface.InterfaceState
 import Domain.GameState
 
 data InterfaceState = InterfaceState { gameState :: GameState
+                                     , frameLength :: Integer
                                      } -- TODO
 
 data Input = NoInput | SomeInput -- TODO
 
-updateInterface :: Input -> InterfaceState -> InterfaceState
-updateInterface NoInput state = state
-updateInterface input state = let newGameState = updateGame DoNothing (gameState state)
-                               in InterfaceState { gameState = newGameState }
+updateInterface :: Input -> Integer -> InterfaceState -> InterfaceState
+updateInterface _ delta state = let newGameState = updateGame DoNothing (gameState state)
+                                    in InterfaceState { gameState = newGameState
+                                                      , frameLength = delta }
--- src/Main.hs
+++ src/Main.hs
@@ -6,31 +6,73 @@ module Main
 ) where
 
 import System.Environment
+import System.Clock
+import Control.Concurrent
 import Interface.InterfaceState
 import Domain.GameState
 
 main :: IO ()
-main = mainLoop initializeState
+main = do
+    startTime <- getTime Monotonic
+    mainLoop (initializeEnvironment startTime) initializeState
 
-mainLoop :: InterfaceState -> IO ()
-mainLoop state = do
-    updatedState <- update state
-    render updatedState
-    if shouldExit updatedState
+
+elapsed :: Environment -> TimeSpec
+elapsed env = diffTimeSpec (current env) (start env)
+
+delta :: Environment -> TimeSpec
+delta env = diffTimeSpec (current env) (previous env)
+
+updateEnvironment :: Environment -> IO Environment
+updateEnvironment env = do
+    newFrame <- getTime (clock env)
+    return Environment { clock = clock env
+                       , start = start env
+                       , current = newFrame
+                       , previous = current env }
+
+mainLoop :: Environment -> InterfaceState -> IO ()
+mainLoop oldEnvironment state = do
+    env <- updateEnvironment oldEnvironment
+    updatedState <- update env state
+    render env updatedState
+    if shouldExit env updatedState
         then return ()
-        else mainLoop updatedState
+        else mainLoop env updatedState
 
-update :: InterfaceState -> IO InterfaceState
-update state = return $ updateInterface NoInput state -- TODO
+update :: Environment -> InterfaceState -> IO InterfaceState
+update env state = do
+    putStrLn "Updating...."
+    threadDelay 100000
+    return $ updateInterface NoInput nanosecs state -- TODO
+    where nanosecs = timeSpecAsNanoSecs $ delta env
 
-render :: InterfaceState -> IO ()
-render state = putStrLn $ (dummyState . gameState) state
+render :: Environment -> InterfaceState -> IO ()
+render env state = do
+    putStrLn "Rendering...."
+    threadDelay 200000
+    putStrLn $ ((dummyState . gameState) state) ++ " " ++ show (frameLength state)
 
-shouldExit :: InterfaceState -> Bool
-shouldExit _ = True -- TODO
+shouldExit :: Environment -> InterfaceState -> Bool
+--shouldExit _ _ = True
+shouldExit env _ = (elapsed env) > (TimeSpec { sec = 3, nsec = 0 })
+
+data Environment = Environment { clock :: Clock
+                               , previous :: TimeSpec
+                               , current :: TimeSpec
+                               , start :: TimeSpec
+                               } deriving (Show)
 
 initializeState :: InterfaceState
 initializeState = InterfaceState { gameState = GameState { zone = Nothing
-                                                         , dummyState = "Nothing to see here yet."
+                                                         , dummyState = "This frame lasted"
                                                          }
+                                 , frameLength = 0
                                  }
+
+initializeEnvironment :: TimeSpec -> Environment
+initializeEnvironment start = Environment { clock = Monotonic
+                                          , current = start
+                                          , previous = start
+                                          , start = start
+                                          }
